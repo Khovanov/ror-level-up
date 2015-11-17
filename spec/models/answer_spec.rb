04f46f0b933c845359f41a764301ab86f5b26551
Stanislav Khovanov <cobweb@mail.ru>
2015-11-17 23:35:23 +0300

    test validates for Answer model: failed

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of :body }
+  it { should validate_length_of(:body).is_at_least(10) }
+
 end
