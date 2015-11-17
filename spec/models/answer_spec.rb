cc5aa4ae29a146642c57e8b13d9ce0dbc7deef75
Stanislav Khovanov <cobweb@mail.ru>
2015-11-18 00:37:43 +0300

    test belong_to for Answer model: failed

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
   it { should validate_length_of(:body).is_at_least(10) }
 
+  it { should belong_to(:question) }
+
 end
