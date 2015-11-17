0e9f190e4e89b7ff9e4cb0035adc26bf79346aa9
Stanislav Khovanov <cobweb@mail.ru>
2015-11-17 23:12:01 +0300

    test validate_length for Question model: failed

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -16,4 +16,7 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
 
+  it { should validate_length_of(:title).is_at_least(10) }
+  it { should validate_length_of(:body).is_at_least(10) }
+
 end
