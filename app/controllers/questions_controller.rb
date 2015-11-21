7790ddd7eb36802a071aa40c6f9048a2d574c709
Stanislav Khovanov <cobweb@mail.ru>
2015-11-21 23:52:50 +0300

    method #new for the Question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,12 @@ class QuestionsController < ApplicationController
   def index 
     @questions = Question.all
   end
+
   def show
     @question = Question.find params[:id]
+  end
+
+  def new
+    @question = Question.new
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do 
     let(:questions) {create_list(:question, 2)}
+    
     before do 
       # @questions = FactoryGirl.create_list(:question, 2)
       # @questions = create_list(:question, 2)
@@ -20,12 +21,26 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'GET #show' do 
     let(:question) {create(:question)}
+
     before {get :show, id: question } # question.id
+
     it 'assigns the requested question to @question' do 
       expect(assigns(:question)).to eq question
     end
     it 'renders show view'do
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do 
+    before {get :new}
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do 
+      expect(response).to render_template :new
     end
     
   end
