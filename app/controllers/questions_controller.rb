b7f80bd12be7a1743e5624e6e93c556104cbe97e
Stanislav Khovanov <cobweb@mail.ru>
2015-11-21 21:29:28 +0300

    method #index to the Questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,5 @@
 class QuestionsController < ApplicationController
+  def index 
+    @questions = Question.all
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,21 +2,20 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do 
-    it 'poulates an array of all questions'do
-      question1 = FactoryGirl.create(:question)
-      question2 = FactoryGirl.create(:question)
-
+    let(:questions) {create_list(:question, 2)}
+    before do 
+      # @questions = FactoryGirl.create_list(:question, 2)
+      # @questions = create_list(:question, 2)
       get :index
-
-      expect(assigns(:questions)).to math_array([question1, question2])
     end
-    it 'renders index view' do 
-      get :index
 
+    it 'poulates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do 
       expect(response).to render_template :index
     end
-    
   end
-
 end
 
