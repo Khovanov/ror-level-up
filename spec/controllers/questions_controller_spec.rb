126459102cb4ee9b4a125e535d3bf3d0599f3ac3
Stanislav Khovanov <cobweb@mail.ru>
2015-11-22 02:35:28 +0300

    method #update for Questions controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -82,5 +82,39 @@ RSpec.describe QuestionsController, type: :controller do
       end
     end
   end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'assigns the requested question to @question' do 
+        patch :update, id: question, question: attributes_for(:question)
+        expect(assigns(:question)).to eq question
+      end 
+
+      it 'change question attributes' do
+        patch :update, id: question, question: { title: 'some test title', body: 'some test body'}
+        question.reload
+        expect(question.title).to eq 'some test title'
+        expect(question.body).to eq 'some test body'
+      end
+
+      it 'redirect to the updated question' do
+        patch :update, id: question, question: attributes_for(:question) 
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      before {patch :update, id: question, question: { title: 'some test title', body: nil}}
+      it 'does not change question attributes' do
+        question.reload
+        expect(question.title).to eq 'MyTitle is to Long'
+        expect(question.body).to eq 'MyText it more 10 symbols'     
+      end
+
+      it 're-render edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
 end
 
