2d92bba99796f7da665261427a6085577cb24cc7
Stanislav Khovanov <cobweb@mail.ru>
2015-12-14 14:38:57 +0300

    add for question accepts nested attributes for attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,8 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   has_many :attachments
   belongs_to :user
+  accepts_nested_attributes_for :attachments
+
   validates :title, :body, :user_id, presence: true 
   validates :title, :body, length: { minimum: 10 }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Question, type: :model do
     it { should belong_to(:user) }
     it { should have_many(:answers).dependent(:destroy) }
     it { should have_many(:attachments) }
+    it { should accept_nested_attributes_for :attachments }
   end
 
   describe 'validates presence of ...' do
