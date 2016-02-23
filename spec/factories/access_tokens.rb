ceec82f22ffba79cddc7e8874d83880580b2de6b
Khovanov <vagrant@rails-dev-box>
2016-02-23 20:02:49 +0000

    api for profile me

---
+++ spec/factories/access_tokens.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :access_token, class: Doorkeeper::AccessToken do
+    application { create(:oauth_application) }
+    resource_owner_id { create(:user).id }
+  end
+end
+
