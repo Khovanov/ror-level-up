4c8f2a3a6707eb23f0cc1e4c4bd6c74b67c43474
Khovanov <vagrant@rails-dev-box>
2016-03-01 23:37:38 +0000

    delayed job and shedule from sidekiq

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,9 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'shoulda/matchers'
 require 'cancan/matchers'
+require 'sidekiq/testing'
 
+Sidekiq::Testing.inline!
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
