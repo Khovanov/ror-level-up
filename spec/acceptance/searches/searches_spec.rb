b0519f45acf46023a56a2e37aab02672b7642ec9
Khovanov <vagrant@rails-dev-box>
2016-03-09 09:46:40 +0000

    refactoring

--- spec/acceptance/searches/searches_spec.rb
+++ spec/acceptance/searches/searches_spec.rb
@@ -4,8 +4,7 @@ require_relative '../sphinx_helper'
 feature 'Search for all or any options', %q(
   Anybody to be able search all or any options
 ) do
-  # let(:rnd) {"testing search#{rnd}" Random::rand(100)}
-  let(:testing_search) {"testing search#{Random::rand(100)}"}
+  let(:testing_search) {"search#{Random::rand(100)}"}
   let!(:questions) { create_list :question, 2 }
   let!(:question) do 
     create :question, 
@@ -28,7 +27,7 @@ feature 'Search for all or any options', %q(
   end
 
   let!(:users) { create_list :user, 2}
-  let!(:user) { create :user, email: 'test@test.com' }
+  let!(:user) { create :user, email: "#{testing_search}@test.com"}
 
   before do
     # ThinkingSphinx::Test.init
@@ -41,111 +40,37 @@ feature 'Search for all or any options', %q(
     # ThinkingSphinx::Test.stop
   end
 
-  context 'search for all' do
-    scenario 'with valid result', sphinx: true  do
-      # ThinkingSphinx::Test.run do
-      visit questions_path
-      fill_in 'query', with: testing_search
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content testing_search
-      # end
-    end
 
-    scenario 'with invalid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: 'anythin'
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content 'No results'
-    end
+  Search::SEARCH_OPTIONS.each do |options|
+    context "search for #{options}" do
+      scenario 'with valid result', sphinx: true  do
+        # ThinkingSphinx::Test.run do
+        visit questions_path
+        fill_in 'query', with: testing_search
+        select(options, from: 'options')        
+        click_on 'Search'
+        expect(current_path).to eq searches_path
+        expect(page).to have_content testing_search
+        # end
+      end
 
-    scenario 'with empty query', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: nil
-      click_on 'Search'
-      expect(current_path).to eq questions_path
-      expect(page).to have_content 'Invalid query'
-    end
-  end
+      scenario 'with invalid result', sphinx: true  do
+        visit questions_path
+        fill_in 'query', with: 'anythin'
+        select(options, from: 'options')
+        click_on 'Search'
+        expect(current_path).to eq searches_path
+        expect(page).to have_content 'No results'
+      end
 
-  context 'search for Questions' do
-    scenario 'with valid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: testing_search
-      select('Questions', from: 'options')
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content testing_search
-    end
-
-    scenario 'with invalid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: 'anythin'
-      select('Questions', from: 'options')
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content 'No results'
-    end
-  end
-
-  context 'search for Answers' do
-    scenario 'with valid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: testing_search
-      select('Answers', from: 'options')
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content testing_search
-    end
-
-    scenario 'with invalid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: 'anythin'
-      select('Answers', from: 'options')
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content 'No results'
-    end
-  end
-
-  context 'search for Comments' do
-    scenario 'with valid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: testing_search
-      select('Comments', from: 'options')
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content testing_search
-    end
-
-    scenario 'with invalid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: 'anythin'
-      select('Comments', from: 'options')
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content 'No results'
-    end
-  end
-
-  context 'search for Users' do
-    scenario 'with valid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: 'test@test.com'
-      select('Users', from: 'options')
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content 'test@test.com'
-    end
-
-    scenario 'with invalid result', sphinx: true  do
-      visit questions_path
-      fill_in 'query', with: 'anythin'
-      select('Users', from: 'options')
-      click_on 'Search'
-      expect(current_path).to eq searches_path
-      expect(page).to have_content 'No results'
+      scenario 'with empty query', sphinx: true  do
+        visit questions_path
+        fill_in 'query', with: nil
+        select(options, from: 'options')
+        click_on 'Search'
+        expect(current_path).to eq questions_path
+        expect(page).to have_content 'Invalid query'
+      end
     end
   end
 end
